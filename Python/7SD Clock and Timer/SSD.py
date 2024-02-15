0cf217bba02191e011e226b238f0104f233db05f
JudKenney <122296864+JudKenney@users.noreply.github.com>
2024-02-15 11:04:36 -0500

    Add files via upload

--- Python/7SD Clock and Timer/SSD.py
+++ Python/7SD Clock and Timer/SSD.py
@@ -1,7 +1,7 @@
 #import libraries
 import RPi.GPIO as GPIO
 import time
-import Keypad
+
 
 #pins for ssd
 segment_pins = [17,4,5,6,13,27,22]
@@ -11,8 +11,6 @@ GPIO.setmode(GPIO.BCM)
 for pin in segment_pins:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.LOW)
-
-
 
 
 digit_segments = {
@@ -32,21 +30,56 @@ digit_segments = {
     'D':[0,1,1,1,1,0,1],
 }
 
+display_on = True
+
+def toggle_display():
+    global display_on
+    display_on = not display_on
+    if not display_on:
+    #turn off all
+        for pin in segment_pins:
+            GPIO.output(pin,GPIO.LOW)
+
+
 def display_number(digit):
-    if digit in digit_segments:
+    global display_on
+    print(f"dislaying: {digit}, dislaystate:{display_on}")
+    if not display_on:
+        print("Display is off, not updating ssd")
+        return
+    if digit =='#':
+        toggle_display()
+    elif digit in digit_segments:
         for pin, state in zip(segment_pins,digit_segments[digit]):
             GPIO.output(pin, GPIO.HIGH if state else GPIO.LOW)
-        else: 
-            for pin in segment_pins:
-                GPIO.output(pin, GPIO.LOW)
+    else: 
+        for pin in segment_pins:
+            GPIO.output(pin, GPIO.LOW)
+
+#GPIO setup for keypad
+X = [18,23,24,25]
+Y = [12,16,20,21]
+
+for x_pin in X:
+    GPIO.setup(x_pin,GPIO.OUT)
+for y_pin in Y:
+    GPIO.setup(y_pin,GPIO.OUT)
+
+def read_key():
+    for rowNum, rowPin in enumerate(X):
+        GPIO.output(rowPin,GPIO.HIGH)
+        for colNum, colPin in enumerate(Y):
+            if GPIO.input(colPin) == 1:
+                GPIO.output(rowPin,GPIO.LOW)
+                return["1","2","3","4","5","6","7","8","9","A","B","C","D"][rowNum*4 + colNum]
+            GPIO.output(rowPin,GPIO.LOW)
+
 
 try:
     while True:
-        key = Keypad.readKey()
-        if key is not None:
-            if key == '#': 
-                display_number(key)
-            else :display_number(key)
+        key = read_key()
+        if key:
+            display_number(key)
             time.sleep(0.5)
 
 except KeyboardInterrupt:
