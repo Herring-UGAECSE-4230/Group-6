4e779aab4b9315f77c32c15c4a3e15adee7eb95b
Yihua Zhang <95250229+RechargeablePaulzyh@users.noreply.github.com>
2024-02-22 09:04:10 -0500

    Add files via upload

--- Python/7SD Clock and Timer/7ssd_should work.py
+++ Python/7SD Clock and Timer/7ssd_should work.py
@@ -1,79 +1,243 @@
+# Imports for file
 import time
 import RPi.GPIO as GPIO
 
-# Defining GPIO mode
+
+
+# Defining GPIO mode and input/output
 GPIO.setmode(GPIO.BCM)
 
-# GPIO pin setup
-# X-Values (Horizontal) for keypad
-X_PINS = [18, 23, 24, 25]  # Yellow, Orange, Brown, Red wires
-# Y-Values (Vertical) for keypad
-Y_PINS = [12, 16, 20, 21]  # Black, White, Gray, Blue wires
-# GPIO pins for the SSD
-SSD_PINS = [17, 4, 6, 13, 19, 27, 22, 5, 26]  # A, B, C, D, E, F, G, DP, Clk
+# X-Values (Horizontal) Mapping
+GPIO.setup(18, GPIO.OUT) # Yellow Wire X1
+GPIO.setup(23, GPIO.OUT) # Orange Wire X2
+GPIO.setup(24, GPIO.OUT) # Brown Wire X3
+GPIO.setup(25, GPIO.OUT) # Red Wire X4
 
-# Setup GPIO pins
-for pin in X_PINS + Y_PINS + SSD_PINS:
-    GPIO.setup(pin, GPIO.OUT if pin in X_PINS or pin in SSD_PINS else GPIO.IN)
+# Y-Values (Vertical) Mapping
+GPIO.setup(12, GPIO.IN) # Black Wire Y1
+GPIO.setup(16, GPIO.IN) # White Wire Y2
+GPIO.setup(20, GPIO.IN) # Gray Wire Y3
+GPIO.setup(21, GPIO.IN) # Blue Wire Y4
 
+# Clock Setup
+Clk1 = 26
+Clk2 = 10
+Clk3 = 9
+Clk4 = 11
+
+# Seven Segment Setup
+E = 13
+D = 6
+C = 5
+DP = 19
+G = 22
+F = 27
+A = 17
+B = 4
+
+LED = 12
+GPIO.setup(Clk1, GPIO.OUT) # Clock 1
+GPIO.setup(Clk2, GPIO.OUT) # Clock 2
+GPIO.setup(Clk3, GPIO.OUT) # Clock 3
+GPIO.setup(Clk4, GPIO.OUT) # Clock 4
+
+GPIO.setup(E, GPIO.OUT) # E
+GPIO.setup(D, GPIO.OUT) # D
+GPIO.setup(C, GPIO.OUT) # C
+GPIO.setup(DP, GPIO.OUT) # DP
+GPIO.setup(G, GPIO.OUT) # G
+GPIO.setup(F, GPIO.OUT) # F
+GPIO.setup(A, GPIO.OUT) # A
+GPIO.setup(B, GPIO.OUT) # B
+GPIO.setup(LED, GPIO.OUT) #LED gpio
+count = 0
 enable = True
+last1 = []
+last2 = []
+last3 = []
+last4 = []
 
-# Mapping of digits to GPIO states for SSD
-DIGIT_MAP = {
-    0: [1,1,1,1,1,1,0],
-    1: [0,1,1,0,0,0,0],
-    2: [1,1,0,1,1,0,1],
-    3: [1,1,1,1,0,0,1],
-    4: [0,1,1,0,0,1,1],
-    5: [1,0,1,1,0,1,1],
-    6: [1,0,1,1,1,1,1],
-    7: [1,1,1,0,0,0,0],
-    8: [1,1,1,1,1,1,1],
-    9: [1,1,1,1,0,1,1]
-}
+# Resets SSD Display
+def resetGPIO(Clk):
+    GPIO.output(Clk, GPIO.HIGH)
+    GPIO.output([A,B,C,D,E,F,G,DP], GPIO.LOW)
+    GPIO.output(Clk, GPIO.LOW)
+    
+def loadLast(Clk, *last):
+    GPIO.output(*last, GPIO.HIGH)
+    GPIO.output(Clk, GPIO.HIGH)
+    GPIO.output(*last, GPIO.LOW)
+    GPIO.output(Clk, GPIO.LOW)
+        
+# Conditional implementation of keypad
+def readKeypad(rowNum, char):
+    curVal = 0
+    GPIO.output(rowNum, GPIO.HIGH)
+    if GPIO.input(12) == 1:
+        curVal = char[0]
+        time.sleep(0.2)
+        return curVal
+        
+    if GPIO.input(16) == 1:
+        curVal = char[1]
+        time.sleep(0.2)
+        return curVal
+        
+    if GPIO.input(20) == 1:
+        curVal = char[2]
+        time.sleep(0.2)
+        return curVal
+        
+    if GPIO.input(21) ==1:
+        curVal = char[3]
+        time.sleep(0.2)
+        return curVal
 
-def reset_ssd():
-    """ Resets the SSD display. """
-    GPIO.output(SSD_PINS[-1], GPIO.HIGH)  # Clk High
-    for pin in SSD_PINS[:-1]:
-        GPIO.output(pin, GPIO.LOW)
-    GPIO.output(SSD_PINS[-1], GPIO.LOW)   # Clk Low
+    GPIO.output(rowNum, GPIO.LOW)
 
-def read_keypad():
-    """ Reads the keypad and returns the pressed key, if any. """
-    key_map = [
-        [1, 2, 3, 'A'],
-        [4, 5, 6, 'B'],
-        [7, 8, 9, 'C'],
-        ['*', 0, '#', 'D']
-    ]
-    for x_pin, row in zip(X_PINS, key_map):
-        GPIO.output(x_pin, GPIO.HIGH)
-        for y_pin, key in zip(Y_PINS, row):
-            if GPIO.input(y_pin) == 1:
-                time.sleep(0.2)
-                GPIO.output(x_pin, GPIO.LOW)
-                return key
-        GPIO.output(x_pin, GPIO.LOW)
-    return None
-
-def display_digit(digit):
-    """ Displays a digit on the SSD. """
-    if digit in DIGIT_MAP:
-        for pin, state in zip(SSD_PINS[:-1], DIGIT_MAP[digit]):
-            GPIO.output(pin, GPIO.HIGH if state else GPIO.LOW)
-        GPIO.output(SSD_PINS[-1], GPIO.HIGH)  # Clk High
+while True:
+    GPIO.output([Clk1,Clk2,Clk3,Clk4], GPIO.LOW)
+    
+    # Clock 1 will be used for row 1
+    row1 = readKeypad(18,[1,2,3,'A'])
+    if row1 == 1 and enable:
+        resetGPIO(Clk1)
+        last1 = [B,C]
+        GPIO.output([B,C], GPIO.HIGH)
+        GPIO.output(Clk1, GPIO.HIGH)
         time.sleep(0.1)
-        GPIO.output(SSD_PINS[-1], GPIO.LOW)   # Clk Low
+        GPIO.output(Clk1, GPIO.LOW)
+        
+    elif row1 == 2 and enable:
+        resetGPIO(Clk1)
+        last1 = [A,B,G,E,D]
+        GPIO.output([A,B,G,E,D], GPIO.HIGH)
+        GPIO.output(Clk1, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(Clk1, GPIO.LOW)
+    elif row1 == 3 and enable:
+        resetGPIO(Clk1)
+        last1 = [A,B,C,G,D]
+        GPIO.output([A,B,C,G,D], GPIO.HIGH)
+        GPIO.output(Clk1, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(Clk1, GPIO.LOW)
+    elif row1 == 'A' and enable:
+        resetGPIO(Clk1)
+        last1 = [F, A, E, B, C, G]
+        GPIO.output([F, A, E, B, C, G], GPIO.HIGH)
+        GPIO.output(Clk1, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(Clk1, GPIO.LOW)
+        
+        
+    # Clock 2 will be used for row 2
+    row2 = readKeypad(23,[4,5,6,'B'])
+    if row2 == 4 and enable:
+        resetGPIO(Clk2)
+        last2 = [F, G, B, C]
+        GPIO.output([F, G, B, C], GPIO.HIGH)
+        GPIO.output(Clk2, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(Clk2, GPIO.LOW)
+    elif row2 == 5 and enable:
+        resetGPIO(Clk2)
+        last2 = [A, F, G, C, D]
+        GPIO.output([A, F, G, C, D], GPIO.HIGH)
+        GPIO.output(Clk2, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(Clk2, GPIO.LOW)
+    elif row2 == 6 and enable:
+        resetGPIO(Clk2)
+        last2 = [A,F,G,C,D,E]
+        GPIO.output([A,F,G,C,D,E], GPIO.HIGH)
+        GPIO.output(Clk2, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(Clk2, GPIO.LOW)
+    elif row2 == 'B' and enable:
+        resetGPIO(Clk2)
+        last2 = [F,E,D,C,G]
+        GPIO.output([F,E,D,C,G], GPIO.HIGH)
+        GPIO.output(Clk2, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(Clk2, GPIO.LOW)
 
-try:
-    while True:
-        reset_ssd()
-        key = read_keypad()
-        if key is not None:
-            if isinstance(key, int) and enable:
-                display_digit(key)
-            elif key == '#':
-                enable = not enable
-except KeyboardInterrupt:
-    GPIO.cleanup()
+    # Clock 3 will be used for row 3
+    row3 = readKeypad(24,[7,8,9,'C'])
+    if row3 == 7 and enable:
+        resetGPIO(Clk3)
+        last3 = [A,B,C]
+        GPIO.output([A,B,C], GPIO.HIGH)
+        GPIO.output(Clk3, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(Clk3, GPIO.LOW)
+    elif row3 == 8 and enable:
+        resetGPIO(Clk3)
+        last3 = [A,B,C,D,E,F,G]
+        GPIO.output([A,B,C,D,E,F,G], GPIO.HIGH)
+        GPIO.output(Clk3, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(Clk3, GPIO.LOW)
+    elif row3 == 9 and enable:
+        resetGPIO(Clk3)
+        last3 = [A,B,C,F,G]
+        GPIO.output([A,B,C,F,G], GPIO.HIGH)
+        GPIO.output(Clk3, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(Clk3, GPIO.LOW)
+    elif row3 == 'C' and enable:
+        resetGPIO(Clk3)
+        last3 = [A,F,E,D]
+        GPIO.output([A,F,E,D], GPIO.HIGH)
+        GPIO.output(Clk3, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(Clk3, GPIO.LOW)
+        
+    #Clock 4 will be used for row 4
+    row4 = readKeypad(25,['*',0,'#','D'])
+    if row4 == '*' and enable:
+        resetGPIO(Clk4)
+        last4 = [DP]
+        GPIO.output([DP], GPIO.HIGH)
+        GPIO.output(Clk4, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(Clk4, GPIO.LOW)
+    elif row4 == 0 and enable:
+        resetGPIO(Clk4)
+        last4 = [A,B,F,C,E,D]
+        GPIO.output([A,B,F,C,E,D], GPIO.HIGH)
+        GPIO.output(Clk4, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(Clk4, GPIO.LOW)
+    elif row4 == '#':
+        resetGPIO(Clk4)
+        print("useless")
+        if(enable):
+                GPIO.output([Clk1,Clk2, Clk3, Clk4], GPIO.HIGH)
+                time.sleep(0.1)
+                GPIO.output([Clk1,Clk2, Clk3, Clk4], GPIO.LOW)
+                enable = False
+                time.sleep(.1)
+                print(enable)
+        else: 
+               enable = True
+               loadLast(Clk1,last1)
+               loadLast(Clk2,last2)
+               loadLast(Clk3, last3)
+               loadLast(Clk4, last4)
+               print(enable)
+    elif row4 == 'D' and enable:
+        resetGPIO(Clk4)
+        if (count % 2 == 0):
+            count = count +1
+            GPIO.output(LED, GPIO.HIGH)
+            time.sleep(.2)
+        else:
+            count = count +1
+            GPIO.output(LED, GPIO.LOW)
+            time.sleep(.2)
+
+        time.sleep(0.1)
+        GPIO.output(Clk4, GPIO.LOW)
+    
+    GPIO.output([Clk1,Clk2,Clk3,Clk4], GPIO.LOW)
